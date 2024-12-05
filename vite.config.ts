03ad8a1dee9a6bcd98943032cb161d6ff1fe74b7
Riya Vishwakarma <riya.vishwakarma@atomostech.com>
2024-12-05 11:30:08 +0530

    publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { peerDependencies } from "./package.json";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: "./src/index.ts",
+      name: "vite-react-ts-button",
+      fileName: (format) => `index.${format}.js`,
+      formats: ["cjs", "es"],
+    },
+    rollupOptions: {
+      external: [...Object.keys(peerDependencies)],
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
   plugins: [react()],
-})
+});
